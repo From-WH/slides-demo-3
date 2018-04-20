4572019130737229c33f7627238f41cbabeb03d1
wanghang <mxw_sunshine@163.com>
2018-04-20 21:21:03 +0800

    苹果轮播-未完全版

--- a.js
+++ a.js
@@ -1,48 +1,53 @@
-let $buttons = $('#buttonWrapper > button')
+let $buttons = $('#buttonWrapper > ul > li > a')
 let $slides = $('#slides')
 let $images = $slides.children('img')
 let current = 0
+
 makeFakeSlides()
-$slides.css({transform: ('translateX(-600px)')})
-
+$slides.css({ transform: ('translateX(-920px)'), transition: `none`})
 bindEvents()
-$(next).on('click',function () {
-    goToSlide(current + 1)
-})
-$(previous).on('click', function () {
-    goToSlide(current - 1)
-})
-let times = setInterval(()=>{
-    goToSlide( current + 1)
-},1500)
 
-$('.container').on('mouseenter',function () {
-    window.clearInterval(times)
-})
-$('.container').on('mouseleave', function () {
-    times = setInterval(() => {
-        goToSlide(current + 1)
-    }, 1500)
-})
+// $(next).on('click',function () {   此处可以设置上下翻页
+//     goToSlide(current + 1)
+// })
+// $(previous).on('click', function () {
+//     goToSlide(current - 1)
+// })
+// document.addEventListener('vis')  //写到这里啦
+// let times = setInterval(()=>{
+//     goToSlide( current + 1)
+// },1500)
+
+// $('.container').on('mouseenter',function () {
+//     window.clearInterval(times)
+// })
+// $('.container').on('mouseleave', function () {
+//     times = setInterval(() => {
+//         goToSlide(current + 1)
+//     }, 2000)
+// })
+
 function bindEvents() {
-    $('#buttonWrapper').on('click', 'button', function (e) {
-        let $button = $(e.currentTarget)
-        let index = $button.index()
+    $('#buttonWrapper').on('click', 'li', function (e) {
+        let $li = $(e.currentTarget)
+        let index = $li.index()
         goToSlide(index)
+
     })
 }
 function goToSlide(index) {
+    $slides.css({ transition: `all 1s` })
     if(index > $buttons.length - 1){
         index = 0
     }else if(index < 0){
         index = $buttons.length - 1
     }
     if (index === 0 && current === $buttons.length - 1) {
-        $slides.css({ transform: `translateX(${-($buttons.length + 1) * 600}px)` })
+        $slides.css({ transform: `translateX(${-($buttons.length + 1) * 920}px)`,background:`red` })
             .one('transitionend', function () {
                 $slides.hide()
                     .offset()
-                $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` })
+                $slides.css({ transform: `translateX(${-(index + 1) * 920}px)` })
                     .show()
             })
     } else if (current === 0 && index === $buttons.length - 1) {
@@ -50,11 +55,11 @@ function goToSlide(index) {
             .one('transitionend', function () {
                 $slides.hide()
                     .offset()
-                $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` })
+                $slides.css({ transform: `translateX(${-(index + 1) * 920}px)` })
                     .show()
             })
     } else {
-        $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` })
+        $slides.css({ transform: `translateX(${-(index + 1) * 920}px)` })
     }
     current = index
 }
@@ -64,4 +69,45 @@ function makeFakeSlides() {
 
         $slides.append($firstCopy)
         $slides.prepend($lastCopy)
+}
+
+
+let $lis = $('#buttonWrapper > ul > li')
+for (let i = 0; i < $lis.length; i++) {
+    $($lis[i]).on('click', function (x) {         //获取每一个span点击信息
+        var index = $(x.currentTarget).index()          //获取点击的是第几个span
+        n = index                                       //将被点击的那个span赋予n
+        activeButton($lis.eq(n)) // siblings接受的是选择器所以有. removeClass接受的是类名所以没有
+    })
+}
+
+var n = 0
+var allImages = $('#slides > img')
+var size = allImages.length
+playSlide(n % size)
+
+var timeID = sitTimer()
+
+
+$('.window').on('mouseenter', function () {
+    window.clearInterval(timeID)
+})
+$('.window').on('mouseleave', function () {
+    timeID = sitTimer()
+})
+
+function sitTimer() {
+    return setInterval(() => {
+        n += 1
+        playSlide(n % size)
+    }, 2000)
+}
+function playSlide(index) {
+    $lis.eq(index).trigger('click')
+}
+
+function activeButton($lis) {
+    $lis
+        .addClass('red')
+        .siblings('.red').removeClass('red')
 }
