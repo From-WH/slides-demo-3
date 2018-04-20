fcfe948d38d605bab5c63e13a281d9718b9de653
wanghang <mxw_sunshine@163.com>
2018-04-20 14:51:59 +0800

    功能完成，未美化

--- a.js
+++ a.js
@@ -1,57 +1,67 @@
 let $buttons = $('#buttonWrapper > button')
 let $slides = $('#slides')
 let $images = $slides.children('img')
-let $firstCopy = $images.eq(0).clone(true)
-let $lastCopy = $images.eq($images.length - 1).clone(true)
-
-$slides.append($firstCopy)
-$slides.prepend($lastCopy)
-
-function makeFakeSlides() {
-    
-}
-
-$slides.css({ transform: ('translateX(-600px)') })
-
 let current = 0
+makeFakeSlides()
+$slides.css({transform: ('translateX(-600px)')})
 
-$buttons.eq(0).on('click', function () {
-    if (current == 3) {
-        $slides.css({ transform: 'translateX(-3000px)' })
-            .one('transitionend', function () {
-                $slides.hide()
-                .offset()
-                $slides.css({ transform: ('translateX(-600px)') })
-                .show()
-            })
-    } else {
-        $slides.css({ transform: 'translateX(-600px)' })
+bindEvents()
+$(next).on('click',function () {
+    goToSlide(current + 1)
+})
+$(previous).on('click', function () {
+    goToSlide(current - 1)
+})
+let times = setInterval(()=>{
+    goToSlide( current + 1)
+},1500)
+
+$('.container').on('mouseenter',function () {
+    window.clearInterval(times)
+})
+$('.container').on('mouseleave', function () {
+    times = setInterval(() => {
+        goToSlide(current + 1)
+    }, 1500)
+})
+function bindEvents() {
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
+}
+function goToSlide(index) {
+    if(index > $buttons.length - 1){
+        index = 0
+    }else if(index < 0){
+        index = $buttons.length - 1
     }
-    current = 0
-})
-$buttons.eq(1).on('click', function () {
-    $slides.css({
-        transform: ('translateX(-1200px)')
-    })
-    current = 1
-})
-$buttons.eq(2).on('click', function () {
-    $slides.css({
-        transform: ('translateX(-1800px)')
-    })
-    current = 2
-})
-$buttons.eq(3).on('click', function () {
-    if (current == 0) {
-        $slides.css({ transform: 'translateX(0px)' })
+    if (index === 0 && current === $buttons.length - 1) {
+        $slides.css({ transform: `translateX(${-($buttons.length + 1) * 600}px)` })
             .one('transitionend', function () {
                 $slides.hide()
                     .offset()
-                $slides.css({ transform: ('translateX(-2400px)') })
+                $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` })
+                    .show()
+            })
+    } else if (current === 0 && index === $buttons.length - 1) {
+        $slides.css({ transform: `translateX(0px)` })
+            .one('transitionend', function () {
+                $slides.hide()
+                    .offset()
+                $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` })
                     .show()
             })
     } else {
-        $slides.css({ transform: ('translateX(-2400px)') })
+        $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` })
     }
-    current = 3
-})
+    current = index
+}
+function makeFakeSlides() {
+        let $firstCopy = $images.eq(0).clone(true)
+        let $lastCopy = $images.eq($images.length - 1).clone(true)
+
+        $slides.append($firstCopy)
+        $slides.prepend($lastCopy)
+}
